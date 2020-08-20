19e4dcf3363043789e5bc9f6f78ba8f4ead72f6e
rahulvictor12 <44474191+rahulvictor12@users.noreply.github.com>
2020-08-20 22:12:01 +0530

    assignment

---
+++ equlibrium index.cpp
@@ -0,0 +1,40 @@
+//equlibrium index
+#include <iostream>
+using namespace std;
+
+int equilibrium_index(int arr[], int n)
+{
+int i, j;
+int leftsum, rightsum;
+
+for (i = 0; i < n; ++i)
+{
+leftsum = 0;
+for (j = 0; j < i; j++)
+leftsum += arr[j];
+
+rightsum = 0;
+for (j = i + 1; j < n; j++)
+rightsum += arr[j];
+
+if (leftsum == rightsum)
+return i;
+}
+
+return -1;
+}
+
+int main()
+{
+int n;
+cout <<"\n Enter the number of elements :" ;
+cin >> n;
+int arr[n];
+cout <<"\nInput the array elements : ";
+for(int i = 0; i < n; i++)
+{
+cin >> arr[i];
+}
+cout <<"\nEquilibrium Index : " << equilibrium_index(arr, n) << endl;
+return 0;
+}
